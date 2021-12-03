5f85705bdc6a8be565fc0d46d622e6d75c3088dd
Gonzalo Montero <gonmon654@gmail.com>
2021-12-03 16:09:53 -0300

    Added Splash Screens

---
+++ FinalDesarrollo2/Assets/Script/Utility/SplashFade.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using TMPro;
+
+using System.Collections;
+
+public class SplashFade : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI splashText;
+    
+    IEnumerator Start()
+    {
+        splashText.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneLoader.GoToMenu();
+    }
+
+    void FadeIn()
+    {
+        splashText.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        splashText.CrossFadeAlpha(0.0f, 1.5f, false);
+    }
+}
---
+++ FinalDesarrollo2/Assets/Script/Utility/SplashFadeImage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashFadeImage : MonoBehaviour
+{
+    [SerializeField] Image splashText;
+
+    IEnumerator Start()
+    {
+        splashText.canvasRenderer.SetAlpha(0.0f);
+
+        FadeIn();
+        yield return new WaitForSeconds(2.5f);
+
+        FadeOut();
+        yield return new WaitForSeconds(2.5f);
+        SceneLoader.GoToMenu();
+    }
+
+    void FadeIn()
+    {
+        splashText.CrossFadeAlpha(1.0f, 1.5f, false);
+    }
+
+    void FadeOut()
+    {
+        splashText.CrossFadeAlpha(0.0f, 1.5f, false);
+    }
+}
