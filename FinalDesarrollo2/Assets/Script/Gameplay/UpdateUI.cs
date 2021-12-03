13d9f3c276c325c651cc412551bc0e5f4bf9e971
Gonzalo Montero <gonmon654@gmail.com>
2021-12-03 00:43:06 -0300

    Added Updating Time UI

---
+++ FinalDesarrollo2/Assets/Script/Gameplay/UpdateUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class UpdateUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI timeText;
+    [SerializeField] TextMeshProUGUI livesText;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    private GameManager gm;
+    private float timePassed;
+
+    void Start()
+    {
+        timePassed = 0;
+        gm = GameManager.Get();
+
+        livesText.text = "Lives: ";
+        //scoreText.text = "Score: " + gm.score;
+    }
+
+    void Update()
+    {
+        timePassed += Time.deltaTime;
+        timeText.text = "Time: " + timePassed.ToString("F2"); 
+    }
+}
